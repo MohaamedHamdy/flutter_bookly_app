c16044071c053a190e6eb40f438718353787075f
MohaamedHamdy <m7mdhamdy45@gmail.com>
2023-08-22 02:46:47 +0300

    create class failure and use dartz package

--- lib/features/home/domain/repo/home_repo.dart
+++ lib/features/home/domain/repo/home_repo.dart
@@ -1,6 +1,9 @@
+import 'package:bookly/core/errors/failure.dart';
 import 'package:bookly/features/home/domain/entities/book_entity.dart';
+import 'package:dartz/dartz.dart';
 
-abstract class HomeRepo{
-  Future<List<BookEntity>> fetchNewsetBooks();
-  Future<List<BookEntity>> fetchFeaturedBooks();
+abstract class HomeRepo {
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks();
+
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks();
 }
